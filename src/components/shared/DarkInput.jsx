34ffc65f2e9ff4e262258fa5b5a0af898279f20f
etobcn <etobcn@yahoo.es>
2026-03-10 10:59:46 +0000

    File changes

--- src/components/shared/DarkInput.jsx
+++ src/components/shared/DarkInput.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-export default function DarkInput({ label, optional, ...props }) {
+export default function DarkInput({ label, optional, id, ...props }) {
   return (
     <div className="space-y-2">
       {label && (
